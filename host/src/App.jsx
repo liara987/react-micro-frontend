db7833cedf29f95bc2a4c76e89889e550bdfea0f
liara987 <987.liara@gmail.com>
2024-12-23 17:34:40 -0300

    fix: add correction to errors

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -4,8 +4,6 @@ import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import ProductList from "./components/ProductList.jsx";
 
-import "./input.css";
-
 const App = () => {
   return (
     <>
